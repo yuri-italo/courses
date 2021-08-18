3e45a0a7c0baf66ceb1b88ec3fc7213f21859e0c
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-18 16:44:24 -0300

    jdbc3 id recuperation done

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/jdbc3/src/application/Main.java
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/jdbc3/src/application/Main.java
@@ -2,9 +2,7 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -21,7 +19,8 @@ public class Main {
                     "INSERT INTO seller "
                     + "(Name, Email, BirthDate, BaseSalary, DepartmentID)"
                     + "VALUES "
-                    + "(?, ?, ?, ?, ?)");
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
             st.setString(1,"Carl Purple");
             st.setString(2,"carl@gmail.com");
@@ -31,7 +30,15 @@ public class Main {
 
             int rowsAffected = st.executeUpdate();
 
-            System.out.println("Done! Rows affected: " + rowsAffected);
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            } else
+                System.out.println("No rows affected!");
+
         } catch (SQLException | ParseException e) {
             e.printStackTrace();
         } finally {
