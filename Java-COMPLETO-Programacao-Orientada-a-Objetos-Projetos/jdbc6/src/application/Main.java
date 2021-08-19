2c4baa1db56cfc4b308fb284d87aeff968a1f378
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-19 11:48:05 -0300

    jdbc6 done

---
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/jdbc6/src/application/Main.java
@@ -0,0 +1,46 @@
+package application;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement st = null;
+
+        try {
+            conn = DB.getConnection();
+            conn.setAutoCommit(false);
+            st = conn.createStatement();
+
+            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
+
+            /*
+            int x = 1;
+            if (x < 2)
+                throw new SQLException("Fake error");
+            */
+            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+            conn.commit();
+
+            System.out.println("rows1 " + rows1);
+            System.out.println("rows2 " + rows2);
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+                throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+            } catch (SQLException ex) {
+                throw new DbException("Error trying to rollback! Caused by: " + ex.getMessage());
+            }
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
