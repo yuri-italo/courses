ce81c2e29644f5b95747bb901ded498042ca31ae
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-23 16:04:28 -0300

    update implementation

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
@@ -33,5 +33,11 @@ public class Main {
         Seller newSeller = new Seller(null, "Greg","greg@gmail.com",new Date(),4000.0,department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
