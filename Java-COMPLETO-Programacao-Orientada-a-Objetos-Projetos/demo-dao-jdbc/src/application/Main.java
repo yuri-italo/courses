986d3c1571e520680f69392c1c46a385cf2e8b65
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-23 11:19:17 -0300

    findById implementation

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
@@ -2,17 +2,13 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        Department obj = new Department(1,"Books");
-        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),300.00,obj);
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
