030073b62fb03708169d6759bc8e0c9b6ddb611a
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-23 15:25:21 -0300

    findAll implementation

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
@@ -21,5 +21,11 @@ public class Main {
         for (Seller obj: list) {
             System.out.println(obj);
         }
+
+        System.out.println("\n=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        for (Seller obj: list) {
+            System.out.println(obj);
+        }
     }
 }
