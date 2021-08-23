24969f7afd70758f8051a7e34caf69b2d1fcabf5
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-23 10:49:27 -0300

    SellerDaoJDBC and DaoFactory

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/demo-dao-jdbc/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),300.00,obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
