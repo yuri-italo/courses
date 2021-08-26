813920dcc7673430fbf040dd5dc0d092514077cf
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-26 13:32:56 -0300

    DepartmentList View design

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/javafx-jdbc/src/main/java/com/example/javafxjdbc/MainViewController.java
+++ Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/javafx-jdbc/src/main/java/com/example/javafxjdbc/MainViewController.java
@@ -32,7 +32,7 @@ public class MainViewController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction() {
-        System.out.println("onMenuItemDepartmentAction");
+        loadView("/com/example/javafxjdbc/DepartmentList.fxml");
     }
 
     @FXML
