a1248c9ab2fc2b4f646665d4df1c89108465a0eb
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-26 10:53:42 -0300

    project removed

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/javafx-jdbc/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.javafxjdbc {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.javafxjdbc to javafx.fxml;
-    exports com.example.javafxjdbc;
-}
