8fa11607b60dd09e8b77f80f1406d0acbba2a165
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-08-27 15:21:29 -0300

    javafx-jdbc done

--- Java-COMPLETO-Programacao-Orientada-a-Objetos-Projetos/javafx-jdbc/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafxjdbc {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.javafxjdbc to javafx.fxml;
-    exports com.example.javafxjdbc;
-}
