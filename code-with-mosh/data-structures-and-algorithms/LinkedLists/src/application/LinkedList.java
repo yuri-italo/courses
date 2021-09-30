131f12f3ce55f0c2e2e3919d893c0fab671b5c5a
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-09-30 17:00:49 -0300

    getKthFromTheEnd() method

--- code-with-mosh/data-structures-and-algorithms/LinkedLists/src/application/LinkedList.java
+++ code-with-mosh/data-structures-and-algorithms/LinkedLists/src/application/LinkedList.java
@@ -123,6 +123,27 @@ public class LinkedList {
         first = previous;
     }
 
+    public int getKthFromTheEnd(int k) {
+        if (isEmpty())
+            throw new IllegalArgumentException();
+
+        var a = first;
+        var b = first;
+
+        for (int i = 0; i < k - 1; i++) {
+            b = b.next;
+            if (b == null)
+                throw new IllegalArgumentException();
+        }
+
+        while (b != last) {
+            a = a.next;
+            b = b.next;
+        }
+
+        return a.value;
+    }
+
     private boolean isEmpty() {
         return first == null;
     }
--- code-with-mosh/data-structures-and-algorithms/LinkedLists/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/LinkedLists/src/application/Main.java
@@ -1,15 +1,13 @@
 package application;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
         var list = new LinkedList();
-        list.addFirst(10);
+        list.addLast(10);
         list.addLast(20);
         list.addLast(30);
-        list.reverse();
-        var array = list.toArray();
-        System.out.println(Arrays.toString(array));
+        list.addLast(40);
+        list.addLast(50);
+        System.out.println(list.getKthFromTheEnd(6));
     }
 }
