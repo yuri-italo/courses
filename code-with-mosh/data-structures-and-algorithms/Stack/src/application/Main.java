83f5f2766b2e864d7bfa99737bc05558ce1e6b43
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-03 10:59:59 -0300

    Implementing a Stack Using an Array

---
+++ code-with-mosh/data-structures-and-algorithms/Stack/src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.peek());
+    }
+}
