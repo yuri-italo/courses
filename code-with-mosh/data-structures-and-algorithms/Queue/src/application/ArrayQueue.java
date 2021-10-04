c296bfb226a5f5816ba30baa91b7a457f7a1adb2
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-04 10:40:05 -0300

    ArrayQueue implemetation

---
+++ code-with-mosh/data-structures-and-algorithms/Queue/src/application/ArrayQueue.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.Arrays;
+
+public class ArrayQueue {
+    private int[] items;
+    private int rear;
+    private int front;
+    private int count;
+
+    public ArrayQueue(int capacity) {
+        items = new int[capacity];
+    }
+
+    public void enqueue(int item) {
+        if (count == items.length)
+            throw new IllegalStateException();
+
+        items[rear] = item;
+        rear = (rear + 1) % items.length;
+        count++;
+    }
+
+    public int dequeue() {
+        var item = items[front];
+        items[front] = 0;
+        front = (front + 1)  % items.length;
+        count--;
+        return item;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        return items[front];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+    public boolean isFull() {
+        return count == items.length;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+}
