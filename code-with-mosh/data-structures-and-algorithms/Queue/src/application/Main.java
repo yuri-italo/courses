c45505e543ff457f3ce5442f9bf9b4bf64701bc9
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-05 09:47:14 -0300

    Implementing a stack using two queues

--- code-with-mosh/data-structures-and-algorithms/Queue/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Queue/src/application/Main.java
@@ -1,17 +1,7 @@
 package application;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        queue.add(40);
-        queue.add(50);
-        QueueReverser.reverse(queue,3);
-        System.out.println(queue);
+
     }
 }
