73ee90f36329f753776a22e8c171d0bf25a939b7
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-06 14:04:08 -0300

    get() method

--- code-with-mosh/data-structures-and-algorithms/HashTables/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/HashTables/src/application/Main.java
@@ -2,8 +2,11 @@ package application;
 
 public class Main {
     public static void main(String[] args) {
-        CharFinder finder = new CharFinder();
-        var ch = finder.findFirstRepeatedChar("green apple");
-        System.out.println(ch);
+        HashTable table = new HashTable();
+        table.put(6, "A");
+        table.put(8, "B");
+        table.put(11, "C");
+        table.put(6, "A+");
+        System.out.println(table.get(6));
     }
 }
