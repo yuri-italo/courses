c85f17b2420fd0d4187ab17196eef7497f3683dd
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-06 14:12:37 -0300

    remove() method

--- code-with-mosh/data-structures-and-algorithms/HashTables/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/HashTables/src/application/Main.java
@@ -7,6 +7,7 @@ public class Main {
         table.put(8, "B");
         table.put(11, "C");
         table.put(6, "A+");
-        System.out.println(table.get(6));
+        table.remove(60);
+        System.out.println(table.get(10));
     }
 }
