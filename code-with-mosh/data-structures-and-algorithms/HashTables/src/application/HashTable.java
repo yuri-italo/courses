0e00642bef7b52217d71b45450cbcf52c688745b
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-06 14:46:21 -0300

    Refactoring

--- code-with-mosh/data-structures-and-algorithms/HashTables/src/application/HashTable.java
+++ code-with-mosh/data-structures-and-algorithms/HashTables/src/application/HashTable.java
@@ -16,47 +16,50 @@ public class HashTable {
     private LinkedList<Entry>[] entries = new LinkedList[5];
 
     public void put(int key, String value) {
-        var index = hash(key);
-
-        if (entries[index] == null)
-            entries[index] = new LinkedList<>();
-
-        var bucket = entries[index];
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                entry.value = value;
-                return;
-            }
+        var entry = getEntry(key);
+        if (entry != null) {
+            entry.value = value;
+            return;
         }
 
-        bucket.addLast(new Entry(key,value));
+        getOrCreateBucket(key).add(new Entry(key,value));
     }
 
     public String get(int key) {
-        var index = hash(key);
-        var bucket = entries[index];
+        var entry = getEntry(key);
 
-        if (bucket != null) {
-            for (var entry : bucket)
-                if (entry.key == key)
-                    return entry.value;
-        }
-        return null;
+        return (entry == null) ? null : entry.value;
     }
 
     public void remove(int key) {
+        var entry = getEntry(key);
+        if (entry == null)
+            throw new IllegalStateException();
+        getBucket(key).remove(entry);
+    }
+
+    private LinkedList<Entry> getBucket(int key) {
+        return entries[hash(key)];
+    }
+
+    private LinkedList<Entry> getOrCreateBucket(int key) {
         var index = hash(key);
         var bucket = entries[index];
-
         if (bucket == null)
-            throw new IllegalStateException();
-        for (var entry : bucket) {
-            if (entry.key == key) {
-                bucket.remove(entry);
-                return;
+            entries[index] = new LinkedList<>();
+
+        return bucket;
+    }
+
+    private Entry getEntry(int key) {
+        var bucket = getBucket(key);
+        if (bucket != null) {
+            for (var entry : bucket) {
+                if (entry.key == key)
+                    return entry;
             }
         }
-       throw new IllegalStateException();
+        return null;
     }
 
     private int hash(int key) {
--- code-with-mosh/data-structures-and-algorithms/HashTables/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/HashTables/src/application/Main.java
@@ -7,7 +7,6 @@ public class Main {
         table.put(8, "B");
         table.put(11, "C");
         table.put(6, "A+");
-        table.remove(60);
-        System.out.println(table.get(10));
+        System.out.println(table.get(6));
     }
 }
