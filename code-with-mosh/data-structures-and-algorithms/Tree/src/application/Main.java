65d3ec40277d4bf25910c2826e9b5caeb1e7bd94
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-11 12:57:18 -0300

    Tree - isBinarySearchTree() method

--- code-with-mosh/data-structures-and-algorithms/Tree/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Tree/src/application/Main.java
@@ -10,15 +10,6 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-
-        Tree tree2 = new Tree();
-        tree2.insert(7);
-        tree2.insert(4);
-        tree2.insert(9);
-        tree2.insert(1);
-        tree2.insert(6);
-        tree2.insert(8);
-        tree2.insert(10);
-        System.out.println(tree.equals(null));
+        System.out.println(tree.isBinarySearchTree());
     }
 }
--- code-with-mosh/data-structures-and-algorithms/Tree/src/application/Tree.java
+++ code-with-mosh/data-structures-and-algorithms/Tree/src/application/Tree.java
@@ -155,6 +155,21 @@ public class Tree {
         return false;
     }
 
+    public boolean isBinarySearchTree() {
+        return isBinarySearchTree(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+    }
+
+    private boolean isBinarySearchTree(Node root, int min, int max) {
+        if (root == null)
+            return true;
+
+        if (root.value < min || root.value > max)
+            return false;
+
+        return isBinarySearchTree(root.leftChild, min, root.value - 1)
+                && isBinarySearchTree(root.rightChild, root.value + 1, max);
+    }
+
     private boolean isLeaf(Node node) {
         return node.leftChild == null && node.rightChild == null;
     }
