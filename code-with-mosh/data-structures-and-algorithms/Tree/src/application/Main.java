c317fe42fde2ca6d49daf8b77fc19e747ff65f6d
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-11 12:43:57 -0300

    Tree - equals() method

--- code-with-mosh/data-structures-and-algorithms/Tree/src/application/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Tree/src/application/Main.java
@@ -10,6 +10,15 @@ public class Main {
         tree.insert(6);
         tree.insert(8);
         tree.insert(10);
-        System.out.println(tree.min());
+
+        Tree tree2 = new Tree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
+        System.out.println(tree.equals(null));
     }
 }
--- code-with-mosh/data-structures-and-algorithms/Tree/src/application/Tree.java
+++ code-with-mosh/data-structures-and-algorithms/Tree/src/application/Tree.java
@@ -136,6 +136,25 @@ public class Tree {
         return Math.min(Math.min(left,right),root.value);
     }
 
+    public boolean equals(Tree other) {
+        if (other == null)
+            return false;
+
+        return equals(root,other.root);
+    }
+
+    private boolean equals(Node first, Node second) {
+        if (first == null && second == null)
+            return true;
+
+        if (first != null && second != null)
+            return first.value == second.value
+                    && equals(first.leftChild, second.leftChild)
+                    && equals(first.rightChild, second.rightChild);
+
+        return false;
+    }
+
     private boolean isLeaf(Node node) {
         return node.leftChild == null && node.rightChild == null;
     }
