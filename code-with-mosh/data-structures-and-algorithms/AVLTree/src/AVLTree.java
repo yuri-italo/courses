4fb6073e52039d882bcec4638bfed055a36f2d5e
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-14 11:40:00 -0300

    AVLTree - insert() method

---
+++ code-with-mosh/data-structures-and-algorithms/AVLTree/src/AVLTree.java
@@ -0,0 +1,34 @@
+public class AVLTree {
+    private class AVLNode {
+       private int value;
+       private AVLNode leftChild;
+        private AVLNode rightChild;
+
+        public AVLNode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + this.value;
+        }
+    }
+
+    private AVLNode root;
+
+    public void insert(int value) {
+        root = insert(root,value);
+    }
+
+    private AVLNode insert(AVLNode root, int value) {
+        if (root == null)
+            return new AVLNode(value);
+
+        if (value < root.value)
+            root.leftChild = insert(root.leftChild, value);
+        else
+            root.rightChild = insert(root.rightChild, value);
+
+        return root;
+    }
+}
---
+++ code-with-mosh/data-structures-and-algorithms/AVLTree/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var tree = new AVLTree();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+    }
+}
