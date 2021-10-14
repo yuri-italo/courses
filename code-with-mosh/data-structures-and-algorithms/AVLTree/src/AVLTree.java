42b7685bd96a87bb8c63ce3047a6ef2e772ec264
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-14 16:45:33 -0300

    AVLTree - Implementing Rotations

--- code-with-mosh/data-structures-and-algorithms/AVLTree/src/AVLTree.java
+++ code-with-mosh/data-structures-and-algorithms/AVLTree/src/AVLTree.java
@@ -30,24 +30,51 @@ public class AVLTree {
         else
             root.rightChild = insert(root.rightChild, value);
 
-        root.height = Math.max(height(root.leftChild),height(root.rightChild)) + 1;
+        setHeight(root);
 
-        balance(root);
-
-        return root;
+        return balance(root);
     }
 
-    private void balance(AVLNode root) {
+    private AVLNode balance(AVLNode root) {
         if (isLeftHeavy(root)) {
             if (balanceFactor(root.leftChild) < 0)
-                System.out.println("Left Rotate " + root.leftChild.value);
-            System.out.println("Right Rotate " + root.value);
+                root.leftChild = rotateLeft(root.leftChild);
+            return rotateRight(root);
         }
         else if (isRightHeavy(root)) {
             if (balanceFactor(root.rightChild) > 0)
-                System.out.println("Right Rotate " + root.rightChild.value);
-            System.out.println("Left Rotate " + root.value);
+                root.rightChild = rotateRight(root.rightChild);
+            return rotateLeft(root);
         }
+        return root;
+    }
+
+    private AVLNode rotateLeft(AVLNode root) {
+        var newRoot = root.rightChild;
+
+        root.rightChild = newRoot.leftChild;
+        newRoot.leftChild = root;
+
+        setHeight(root);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+
+    private AVLNode rotateRight(AVLNode root) {
+        var newRoot = root.leftChild;
+
+        root.leftChild = newRoot.rightChild;
+        newRoot.rightChild = root;
+
+        setHeight(root);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+
+    private void setHeight(AVLNode node) {
+        node.height = Math.max(height(node.leftChild),height(node.rightChild)) + 1;
     }
 
     private boolean isLeftHeavy(AVLNode node) {
