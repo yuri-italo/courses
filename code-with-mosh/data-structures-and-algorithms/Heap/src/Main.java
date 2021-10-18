c0f4cba3feab09a44fa180c2dee3304522eb5c6c
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-18 15:34:03 -0300

    Heap - Heapify

--- code-with-mosh/data-structures-and-algorithms/Heap/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Heap/src/Main.java
@@ -1,12 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        var heap = new Heap();
-        heap.insert(10);
-        heap.insert(5);
-        heap.insert(17);
-        heap.insert(4);
-        heap.insert(22);
-        heap.remove();
-        System.out.println("Done");
+        int[] numbers = {5,3,8,4,1,2};
+        MaxHeap.heapify(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
