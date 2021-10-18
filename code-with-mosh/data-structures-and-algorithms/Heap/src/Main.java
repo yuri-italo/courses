f7a97f25e740bbc20a50b1dd56e72a168a93c0d8
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-18 10:47:54 -0300

    Heap - insert() method

---
+++ code-with-mosh/data-structures-and-algorithms/Heap/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var heap = new Heap();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(17);
+        heap.insert(4);
+        heap.insert(22);
+        System.out.println("Done");
+    }
+}
