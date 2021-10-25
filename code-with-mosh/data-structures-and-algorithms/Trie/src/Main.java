21d4d9e73112a1a53726d81fff7ce4371cddbc2d
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-25 08:36:52 -0300

    Trie - insert() method

---
+++ code-with-mosh/data-structures-and-algorithms/Trie/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var trie = new Trie();
+        trie.insert("cat");
+        trie.insert("can");
+        System.out.println("Done");
+    }
+}
