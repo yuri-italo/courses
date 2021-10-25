70a0dc2cf417a5e1c0e42cd2cc2fcc3d05da12d8
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-25 08:55:29 -0300

    HashTable implementation and abstraction

--- code-with-mosh/data-structures-and-algorithms/Trie/src/Trie.java
+++ code-with-mosh/data-structures-and-algorithms/Trie/src/Trie.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
+
 public class Trie {
     public static int ALPHABET_SIZE = 26;
 
     private class Node {
         private char value;
-        private Node[] children = new Node[ALPHABET_SIZE];
+        private HashMap<Character,Node> children = new HashMap<>();
         private boolean isEndOfWord;
 
         public Node(char value) {
@@ -14,6 +16,18 @@ public class Trie {
         public String toString() {
             return "value=" + value;
         }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return  children.get(ch);
+        }
     }
 
     private Node root = new Node(' ');
@@ -21,10 +35,9 @@ public class Trie {
     public void insert(String word) {
         var current = root;
         for (var ch : word.toCharArray()) {
-            var index = ch - 'a';
-            if (current.children[index] == null)
-                current.children[index] = new Node(ch);
-            current = current.children[index];
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
         }
         current.isEndOfWord = true;
     }
