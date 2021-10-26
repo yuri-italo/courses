9810f032d65fddfa57d3b0e42b38ee59cccb9409
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-26 13:18:29 -0300

    Graph - hasCycle() method (DIRECTED GRAPH)

--- code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
@@ -1,15 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         var graph = new Graph();
-        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("P");
-        graph.addEdge("X","A");
-        graph.addEdge("X","B");
-        graph.addEdge("A","P");
-        graph.addEdge("B","P");
-        var list = graph.topologicalSort();
-        System.out.println(list);
+        graph.addNode("C");
+        graph.addEdge("A","B");
+        graph.addEdge("B","C");
+        graph.addEdge("C","A");
+        System.out.println(graph.hasCycle());
     }
 }
