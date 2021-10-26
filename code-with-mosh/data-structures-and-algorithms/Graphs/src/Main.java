c413d149d690827f665c4edcab8a2a9a82e659f2
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-26 12:05:28 -0300

    Graph - topologicalSort() method

--- code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         var graph = new Graph();
+        graph.addNode("X");
         graph.addNode("A");
         graph.addNode("B");
-        graph.addNode("C");
-        graph.addNode("D");
-        graph.addEdge("A","B");
-        graph.addEdge("B","D");
-        graph.addEdge("D","C");
-        graph.addEdge("A","C");
-        graph.traverseBreadthFirst("K");
+        graph.addNode("P");
+        graph.addEdge("X","A");
+        graph.addEdge("X","B");
+        graph.addEdge("A","P");
+        graph.addEdge("B","P");
+        var list = graph.topologicalSort();
+        System.out.println(list);
     }
 }
