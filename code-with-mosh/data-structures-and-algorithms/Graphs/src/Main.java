81862b1f90cd7566b4b5cac2444ab1635b74340d
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-27 09:34:37 -0300

    WeightedGraph - hasCycle() method (Undirected Graphs)

--- code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
@@ -4,10 +4,9 @@ public class Main {
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("A","B",1);
-        graph.addEdge("B","C",2);
-        graph.addEdge("A","C",10);
-        var path = graph.getShortestPath("C","A");
-        System.out.println(path);
+        graph.addEdge("A","B",0);
+        graph.addEdge("B","C",0);
+        graph.addEdge("C","A",0);
+        System.out.println(graph.hasCycle());
     }
 }
