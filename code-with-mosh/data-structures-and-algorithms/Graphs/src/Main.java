06445ae551cacf1222c54781a78096ddebba40c9
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-27 10:18:42 -0300

    WeightedGraph - getMinimumSpanningTree() method - Prim's Algorithm

--- code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
@@ -4,9 +4,13 @@ public class Main {
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("A","B",0);
-        graph.addEdge("B","C",0);
-        graph.addEdge("C","A",0);
-        System.out.println(graph.hasCycle());
+        graph.addNode("D");
+        graph.addEdge("A","B",3);
+        graph.addEdge("B","D",4);
+        graph.addEdge("C","D",5);
+        graph.addEdge("A","C",1);
+        graph.addEdge("B","C",2);
+        var tree = graph.getMinimumSpanningTree();
+        tree.print();
     }
 }
