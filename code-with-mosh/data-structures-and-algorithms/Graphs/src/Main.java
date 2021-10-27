24e22d9d509394a9bbf4f35486d1d7bbacfd2ffb
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-27 07:54:46 -0300

    Weighted Undirected Graph

--- code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Graphs/src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        var graph = new Graph();
+        var graph = new WeightedGraph();
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("A","B");
-        graph.addEdge("B","C");
-        graph.addEdge("C","A");
-        System.out.println(graph.hasCycle());
+        graph.addEdge("A","B",3);
+        graph.addEdge("A","C",2);
+        graph.print();
     }
 }
---
+++ code-with-mosh/data-structures-and-algorithms/Graphs/src/WeightedGraph.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WeightedGraph {
+    private class Node {
+        private String label;
+
+        public Node(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+
+    private class Edge {
+        private Node from;
+        private Node to;
+        private int weight;
+
+        public Edge(Node from, Node to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public String toString() {
+            return from + "->" + to;
+        }
+    }
+
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Edge>> adjacencyList = new HashMap<>();
+
+    public void addNode(String label) {
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(String from, String to, int weight) {
+        var fromNode = nodes.get(from);
+        if (fromNode == null)
+            throw  new IllegalArgumentException();
+
+        var toNode = nodes.get(to);
+        if (toNode == null)
+            throw  new IllegalArgumentException();
+
+        adjacencyList.get(fromNode).add(new Edge(fromNode, toNode, weight));
+
+        adjacencyList.get(toNode).add(new Edge(toNode, fromNode, weight));
+    }
+
+    public void print() {
+        for (var source : adjacencyList.keySet()) {
+            var targets = adjacencyList.get(source);
+            if (!targets.isEmpty())
+                System.out.println(source + " is connected to " + targets);
+        }
+    }
+
+}
