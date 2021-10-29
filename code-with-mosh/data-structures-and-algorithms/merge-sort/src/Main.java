9fd0dca66ad5561de70278b031de8bcbd6c15c09
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-29 10:09:20 -0300

    Merge Sort implementation

---
+++ code-with-mosh/data-structures-and-algorithms/merge-sort/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {8,3,1,7,10,5,4,9,8,2,5,6};
+        System.out.println(Arrays.toString(array));
+        var sorter = new MergeSort();
+        sorter.sort(array);
+        System.out.println("Sorted: " + Arrays.toString(array));
+    }
+}
