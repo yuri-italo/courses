a767666c278d0510ead31648aa3f29c1563f3d39
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-10-29 11:15:33 -0300

    Counting Sort implementation

---
+++ code-with-mosh/data-structures-and-algorithms/counting-sort/src/CountingSort.java
@@ -0,0 +1,12 @@
+public class CountingSort {
+    public void sort(int[] array, int max) {
+        int[] counts = new int[max + 1];
+        for (var item : array)
+            counts[item]++;
+
+        var k = 0;
+        for (int i = 0; i < counts.length; i++)
+            for (int j = 0; j < counts[i]; j++)
+                array[k++] = i;
+    }
+}
