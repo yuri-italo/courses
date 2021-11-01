3227d0610f4ebd46e5a14cdef6440a2de56dbbf2
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-11-01 09:50:16 -0300

    Search - Ternary search implementation (Recursive)

--- code-with-mosh/data-structures-and-algorithms/Search/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Search/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Search search = new Search();
         int[] numbers = {1,3,5,6,7};
-        System.out.println(search.binarySearch(numbers,30));
+        System.out.println(search.ternarySearch(numbers,50));
     }
 }
