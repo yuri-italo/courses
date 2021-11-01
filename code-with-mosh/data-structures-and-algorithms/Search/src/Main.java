829a43756ae49f38d0001d945447768818e2ce0f
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-11-01 09:26:03 -0300

    Search - Binary search implementation (Iterative)

--- code-with-mosh/data-structures-and-algorithms/Search/src/Main.java
+++ code-with-mosh/data-structures-and-algorithms/Search/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Search search = new Search();
         int[] numbers = {1,3,5,6,7};
-        System.out.println(search.binarySearchRec(numbers,1));
+        System.out.println(search.binarySearch(numbers,30));
     }
 }
--- code-with-mosh/data-structures-and-algorithms/Search/src/Search.java
+++ code-with-mosh/data-structures-and-algorithms/Search/src/Search.java
@@ -24,4 +24,23 @@ public class Search {
 
         return binarySearchRec(array,target,middle + 1, right);
     }
+
+    public int binarySearch(int[] array, int target) {
+        var left = 0;
+        var right = array.length -1;
+
+        while (left <= right) {
+          var middle = (left + right) / 2;
+
+          if (array[middle] == target)
+              return middle;
+
+          if (target < array[middle])
+              right = middle - 1;
+          else
+              left = middle + 1;
+        }
+
+        return -1;
+    }
 }
