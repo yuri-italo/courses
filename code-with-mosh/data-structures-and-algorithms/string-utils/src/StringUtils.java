3432353117ee8ff4b62c429b14776c92c73daa67
Yuri Italo Sant'Ana <yuri.italo94@gmail.com>
2021-11-02 09:30:51 -0300

    String - reverse() method

--- code-with-mosh/data-structures-and-algorithms/string-utils/src/StringUtils.java
+++ code-with-mosh/data-structures-and-algorithms/string-utils/src/StringUtils.java
@@ -11,4 +11,16 @@ public class StringUtils {
 
         return qtt;
     }
+
+    public static String reverse(String str) {
+        if (str == null)
+            return "";
+
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0 ; i--) {
+            reversed.append(str.charAt(i));
+        }
+
+        return reversed.toString();
+    }
 }
